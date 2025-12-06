9be4d73677ff21e30dc4c192c32d0c454ab101c0
manpreetwantstolearn <msmanpreet2610@gmail.com>
2025-12-06 18:33:14 +0530

    Changed other directory structures also

--- exception/src/ZenithException.cpp
+++
@@ -1,8 +0,0 @@
-#include "exception/ZenithException.hpp"
-
-namespace exception {
-
-ZenithException::ZenithException(const std::string& message)
-    : std::runtime_error(message) {}
-
-} // namespace exception
